2ab9f278fa70a436ce23f4a04a5adcff476542ca
kaarthikrajamv <kaarthikrajamv@hotmail.com>
2017-06-01 16:42:55 +0530

    Day1_Complete

--- Day1/Assignment_Morning.py
+++ Day1/Assignment_Morning.py
@@ -15,10 +15,60 @@
 
 # s=raw_input("Enter String to Reverse:")
 # print s, s[::-1],s[-4:]
-a=[2,3,4,5,7]
-sum=0
-b=[]
-for i in a:
-	sum+=i
-	b.append(sum)
-print a,b
+# a=[2,3,4,5,7]
+# a=a+input('Enter list')
+	
+# sum=0
+# b=[]
+# for i in a:
+# 	sum+=i
+# 	b.append(sum)
+# print a,b
+
+# help(dict)
+# d={}
+# # l=input("Enter List");
+# l=['qwerty','asdfg','sfa','EFAF']
+# for i in l:
+# 	d[i]=len(i)
+# c={l[i]: len(l[i]) for i in range(len(l))}
+# print d,c
+# n=input("Enter Numbers : ")
+# k=input("Power : ")
+# print [tuple([i**j for j in range(1,k+1) ]) for i in range(0,n+1)]
+# l=input("List : ")+[1,2,3,4,5,6,7,8,9]
+# sum=0
+# for i in l:
+# 	if i%2==0:
+# 		sum+=i
+# print(sum)
+# i1,i2,i3=[int(x) for x in raw_input("Enter three nos : ").split(" ")]
+# print i1,i2,i3
+# max,min=i1,i1;
+# if i2>i1:
+# 	if i3>i2:
+# 		max=i3
+# 	else:
+# 		max=i2
+# else:
+# 	if i3<i2:
+# 		min=i3
+# 	else :
+# 		min-i2
+# print max,"and",min
+# a,b=[int(x) for x in raw_input("Enter two nos : ").split(" ")]
+# print a**b
+# n=input("Triangle No : ")
+# # n=7
+# for i in range(1,n+1):
+# 	for j in range(1,i+1):
+# 		print j,
+# 	print ""
+
+# s=[x for x in list(raw_input("Enter a String ").lower()) if x.isalpha()]
+# rev=s[:]
+# rev.reverse()
+# print s==rev
+# print 27==len(set(raw_input("Enter Sentence :").lower()))
+# print list(raw_input("Enter two Strings : ")).sort()==list(raw_input()).sort()
+# print a==b
