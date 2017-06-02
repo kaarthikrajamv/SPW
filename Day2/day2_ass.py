f3507ab850ba9d50d25e5fe4b02ad3e3feee91d7
kaarthikrajamv <kaarthikrajamv@hotmail.com>
2017-06-02 16:47:41 +0530

    Day2_full

--- Day2/day2_ass.py
+++ Day2/day2_ass.py
@@ -60,3 +60,82 @@
 # print reduce(lambda x,y:x if x>y else y,[1,3,4,2,6])
 # print reduce(lambda x,y:max(x,y),[1,3,4,2,6])
 #
+# print [(x,y,(x*x+y*y)**0.5) for x in range(1,51) for y in range(1,51) if (x*x+y*y)**0.5==(int((x*x+y*y)**0.5))  ]
+# 
+m1=[[1,2,3],[2,4,5]]
+m2=[[3,4],[3,6],[6,1]]
+
+
+# input("m1:")
+# input("m2:")
+# if(len(m2)!=len(m1[0])):
+# 	print "Incompatible"
+# else:
+# 	mr=[[0 for j in range (len(m2[0])) ] for i in range(len(m1)) ]
+# 	mr = [[sum(m1[i][k]*m2[k][j] for k in range(len(m2))) for j in range(len(m2[0]))] for i in range(len(m1))]
+# 	print mr
+
+
+# try: 
+# 	print 1/0
+# except :
+# 	print  
+# finally:
+# 	print "In finally"
+# print "I too tried it"
+# fin =open('sampleawe4y.txt','r')
+
+
+ # 	line =fin.readline()
+	# line=line.strip()
+	# line=list(line)
+	# line=[x for x in line if x.isalpha()]
+	# print line
+	# rev=line[:]
+	# rev.reverse()
+	# if line==rev:
+	# 	print "Palind"
+	# else:
+		# print "No Palind"
+# d={i:0 for i in range (1,20)}
+
+# try:
+# 	fin = None
+# 	fin =open('palind.txt','r')
+# 	for i in fin.readlines():
+# 		for j in i.split():
+# 			d[len(j)]+=1
+
+# except IOError:
+# 	# print fin
+# 	print("File Not Found")
+# except TypeError:
+# 	print("Invalid File Error")
+# finally:
+# 	print
+# 	if fin:
+# 		fin.close()
+# print d.items()
+d={}
+
+try:
+	fin = None
+	fin =open('sample.txt','r')
+	for i in fin.readlines():
+		for j in i.split():
+			if d.has_key(j):
+				d[j]+=1
+			else :
+				d[j]=1
+
+
+except IOError:
+	# print fin
+	print("File Not Found")
+except TypeError:
+	print("Invalid File Error")
+finally:
+	print
+	if fin:
+		fin.close()
+print {k:v for k,v in d.items() if v==1}.keys()
